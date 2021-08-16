2327f2a465560a6d3a6af0a896c2ef11c505e2ea
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-08-16 14:19:50 +0300

    changes

--- services/contacts.js
+++ services/contacts.js
@@ -1,8 +1,13 @@
 const { Contact } = require("../models");
 
-// const getAll = () => {
-//   return Contact.find({});
-// };
+const getAll = () => {
+  return Contact.find({}, "_id name email phone");
+};
+
+const getById = (id) => {
+  Contact.findOne({ _id: id });
+  return Contact.findById(id);
+};
 
 const add = (newContact) => {
   return Contact.create(newContact);
@@ -10,5 +15,6 @@ const add = (newContact) => {
 
 module.exports = {
   add,
-  // getAll,
+  getAll,
+  getById,
 };
